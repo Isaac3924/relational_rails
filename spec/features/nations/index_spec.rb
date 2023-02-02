8ad6b9a3a4ff9d6c05900e37689c494d8daface9
Isaac Alter <i_alter@yahoo.com>
2023-02-02 13:09:48 -0700

    Adds refactor: Iteration 1 User Story 1 supporting rspec testing

---
+++ spec/features/nations/index_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Nations index' do
+
+  it 'shows all of the names of the nations' do
+		nation = Nation.create!(name: "Gahlland", landlocked: false, population: 324, national_language: "Gahlish")
+    nation2 = Nation.create!(name: "Bjornsval", landlocked: false, population: 212, national_language: "Bjornish")
+    visit "/nations"
+    save_and_open_page
+    expect(page).to have_content(nation.name)
+    expect(page).to have_content(nation2.name)
+  end
+end
