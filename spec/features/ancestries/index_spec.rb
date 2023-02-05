89ee8438a89d76ac8c05fedd4e7414ebfaf019f6
Isaac Alter <i_alter@yahoo.com>
2023-02-05 12:38:16 -0700

    Adds refactor: Iteration 1 User Story 8 passing test on ancestries index spec COMPLETE

--- spec/features/ancestries/index_spec.rb
+++ spec/features/ancestries/index_spec.rb
@@ -62,8 +62,20 @@ RSpec.describe 'the ancestries index page' do
     ancestry = nation.ancestries.create!(name: "Elves", darkvision: false, population: 1500, patron_deity: "Faesren")
     ancestry2 = nation2.ancestries.create!(name: "Orcs", darkvision: true, population: 32000, patron_deity: "Orrug")
     visit "/ancestries"
-    save_and_open_page
+
     expect(page).to have_content(ancestry.nation_id)
     expect(page).to have_content(ancestry2.nation_id)
+  end
+
+  it 'has a link that leads to the child index' do
+    nation = Nation.create!(name: "Gahlland", landlocked: false, population: 324, national_language: "Gahlish")
+    nation2 = Nation.create!(name: "Bjornsval", landlocked: false, population: 212, national_language: "Bjornish")
+    ancestry = nation.ancestries.create!(name: "Elves", darkvision: false, population: 1500, patron_deity: "Faesren")
+    ancestry2 = nation2.ancestries.create!(name: "Orcs", darkvision: true, population: 32000, patron_deity: "Orrug")
+    visit "/ancestries"
+
+    click_on "Ancestries Index"
+    save_and_open_page
+    expect(current_path).to eq ("/ancestries")
   end
 end
