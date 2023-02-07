9ddca55b526c2a44c9b2b61756c09a340b8f3fa2
Isaac Alter <i_alter@yahoo.com>
2023-02-07 15:17:19 -0700

    Adds feat: Iteration 2 User Story 21 COMPLETE PREVIOUSLY User Story 21 basic framework set

--- spec/features/nations/destroy_spec.rb
+++ spec/features/nations/destroy_spec.rb
@@ -10,8 +10,9 @@ require 'rails_helper'
 
 RSpec.describe 'destroying a nation (which sounds METAL)' do
   before :each do
-    @nation = Nation.create!(name: "delete_me", landlocked: false, population: 1, national_language: "DO IT")
-    @ancestry = @nation.ancestries.create!(name: "Destroy", darkvision: true, population: 2, patron_deity: "Nihil")
+    @nation = Nation.create!(name: "delete_me1", landlocked: false, population: 1, national_language: "DO IT")
+    @nation2 = Nation.create!(name: "delete_me2", landlocked: false, population: 1, national_language: "DO IT")
+    @ancestry = @nation.ancestries.create!(name: "Destroy1", darkvision: true, population: 2, patron_deity: "Nihil")
     @ancestry2 = @nation.ancestries.create!(name: "Destroy2", darkvision: true, population: 3, patron_deity: "Orrug")
   end
 
@@ -24,27 +25,61 @@ RSpec.describe 'destroying a nation (which sounds METAL)' do
   it 'can destroy the nation' do
     visit "/nations/#{@nation.id}"
 
-    expect(page).to have_content('delete_me')
+    expect(page).to have_content('delete_me1')
 
     click_link "Delete #{@nation.name}"
     save_and_open_page
       
     expect(current_path).to eq("/nations")
-    expect(page).to_not have_content("delete_me")
+    expect(page).to_not have_content("delete_me1")
   end
 
   it 'can destroy the ancestries' do
     visit "/ancestries"
-    expect(page).to have_content('Destroy')
+    expect(page).to have_content('Destroy1')
     expect(page).to have_content('Destroy2')
 
     visit "/nations/#{@nation.id}"
     click_link "Delete #{@nation.name}"
     visit "/ancestries"
-    save_and_open_page
-      
+    
     expect(current_path).to eq("/ancestries")
-    expect(page).to_not have_content("Destroy")
+    expect(page).to_not have_content("Destroy1")
     expect(page).to_not have_content("Destroy2")
   end
+  
+  it 'has links indicating deletion on index page' do
+    visit "/nations"
+    
+    save_and_open_page
+    expect(page).to have_content("Delete #{@nation.name}")
+    expect(page).to have_content("Delete #{@nation2.name}")
+  end
+    
+  # it 'can destroy the nation' do
+  #   visit "/nations/#{@nation.id}"
+
+  #   expect(page).to have_content('delete_me')
+
+  #   click_link "Delete #{@nation.name}"
+  #   save_and_open_page
+      
+  #   expect(current_path).to eq("/nations")
+  #   expect(page).to_not have_content("delete_me")
+  # end
+
+  # it 'can destroy the ancestries' do
+  #   visit "/ancestries"
+  #   expect(page).to have_content('Destroy')
+  #   expect(page).to have_content('Destroy2')
+
+  #   visit "/nations/#{@nation.id}"
+  #   click_link "Delete #{@nation.name}"
+  #   visit "/ancestries"
+  #   save_and_open_page
+      
+  #   expect(current_path).to eq("/ancestries")
+  #   expect(page).to_not have_content("Destroy")
+  #   expect(page).to_not have_content("Destroy2")
+  # end
 end
