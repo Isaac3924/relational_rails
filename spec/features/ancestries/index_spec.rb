f41fbd3c4d038102e7582ffc657e12300de4bc7f
Isaac Alter <i_alter@yahoo.com>
2023-02-07 09:21:13 -0700

    Adds feat: Iteration 2 User Story 18 ancestry index and spec complete

--- spec/features/ancestries/index_spec.rb
+++ spec/features/ancestries/index_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'the ancestries index page' do
     @nation2 = Nation.create!(name: "Bjornsval", landlocked: false, population: 212, national_language: "Bjornish")
     @ancestry = @nation.ancestries.create!(name: "Elves", darkvision: false, population: 1500, patron_deity: "Faesren")
     @ancestry2 = @nation2.ancestries.create!(name: "Orcs", darkvision: true, population: 32000, patron_deity: "Orrug")
+    @ancestry3 = @nation.ancestries.create!(name: "Dwarves", darkvision: true, population: 4600, patron_deity: "Dorwun")
   end
 
   it 'displays all ancestries names' do
@@ -60,8 +61,22 @@ RSpec.describe 'the ancestries index page' do
   it 'has a link that leads to the parent index' do
     visit "/ancestries"
 
-    save_and_open_page
     click_on "Nations Index"
     expect(current_path).to eq ("/nations")
+  end
+
+  it 'has links to edit ancestries' do
+    visit "/ancestries"
+
+    expect(page).to have_content("Update #{@ancestry2.name}")
+    expect(page).to have_content("Update #{@ancestry3.name}")
+  end
+
+  it 'has link that leads to update page' do
+    visit "/ancestries"
+
+    click_link "Update #{@ancestry2.name}"
+    save_and_open_page
+    expect(current_path).to eq ("/ancestries/#{@ancestry2.id}/edit")
   end
 end
